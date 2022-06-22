8b5e84d090cdcba7a31e55f46275f52c976a434c
IuliiaPi <julia.pidlisna@gmail.com>
2022-06-22 10:36:42 -0400

    feat: implement reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let abs = Math.abs(n);
+    let numToStr = abs.toString();
+    let strToArray = numToStr.split("");
+    let reverseArray = strToArray.reverse();
+    let arrayToStr = reverseArray.join("");
+    let strToNum = Number(arrayToStr);
+    let result = strToNum; // * Math.sign(n);
+    return result;
 }
